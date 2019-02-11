418b0f346abaf92864a9f4218fa2f842072a40de
Martin Cerny <martin.cerny@xbim.cz>
2019-02-11 10:40:33 +0100

    Add project files.

---
+++ DDSS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DDSS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
