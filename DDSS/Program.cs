69b8f82e24b9ecb2f6603ce4944a72bdd808a716
Martin Cerny <martin.cerny@xbim.cz>
2019-06-03 15:11:38 +0200

    Units map and enums map added. Model validated before save.

--- DDSS/Program.cs
+++ DDSS/Program.cs
@@ -1,4 +1,8 @@
-﻿using Xbim.Common.Step21;
+﻿using DDSS.Utils;
+using Serilog;
+using System.IO;
+using Xbim.Common;
+using Xbim.Common.Step21;
 using Xbim.Ifc;
 using Xbim.Ifc4.ActorResource;
 using Xbim.Ifc4.ExternalReferenceResource;
@@ -16,6 +20,8 @@ namespace DDSS
     {
         static void Main(string[] args)
         {
+            const string ifcFile = "DDSS.ifc";
+
             var editor = new XbimEditorCredentials
             {
                 ApplicationFullName = "Databáze datového standardu stavebnictví",
@@ -27,12 +33,27 @@ namespace DDSS
                 EditorsOrganisationName = "Česká agentura pro standardizaci"
             };
 
+            // set up logger to file and console
+            var logFile = Path.ChangeExtension(ifcFile, ".log");
+            Log.Logger = new LoggerConfiguration()
+               .Enrich.FromLogContext()
+               .WriteTo.Console()
+               .WriteTo.File(logFile)
+               .CreateLogger();
+            XbimLogging.LoggerFactory.AddSerilog();
+
             IfcStore.ModelProviderFactory.UseMemoryModelProvider();
             using (var model = IfcStore.Create(editor, XbimSchemaVersion.Ifc4, XbimStoreType.InMemoryModel))
             { 
                 var i = model.Instances;
                 using (var txn = model.BeginTransaction())
                 {
+                    // these will keep cache of units and enums
+                    // units have to be defined in the source of 'UnitMap' (compile time) while
+                    // enumerations are to be defined in runtime based on the values in the DB
+                    var units = new UnitMap(model);
+                    var enums = new EnumsMap(model);
+
                     // LOIN = level of information need for specific purpose, role, milestone and classification
                     // root object type, there might be many of them
                     var loin = i.New<IfcProjectLibrary>(lib =>
@@ -44,6 +65,7 @@ namespace DDSS
                     i.New<IfcRelAssignsToControl>(rel =>
                     {
                         rel.RelatedObjects.Add(loin);
+                        rel.RelatedObjectsType = IfcObjectTypeEnum.NOTDEFINED;
                         rel.RelatingControl = i.New<IfcActionRequest>(r =>
                         {
                             r.Name = "Kontrola zadaných parametrů";
@@ -52,10 +74,11 @@ namespace DDSS
                         });
                     });
 
-                    // Actor / Role = Who in interested in the data
+                    // Actor / Role = Who is interested in the data
                     i.New<IfcRelAssignsToActor>(r =>
                     {
                         r.ActingRole = i.New<IfcActorRole>(ar => ar.Role = IfcRoleEnum.CLIENT);
+                        r.RelatedObjectsType = IfcObjectTypeEnum.NOTDEFINED;
                         r.RelatedObjects.Add(loin);
                         r.RelatingActor = i.New<IfcActor>(a =>
                         {
@@ -70,6 +93,7 @@ namespace DDSS
                     i.New<IfcRelAssignsToProcess>(rel =>
                     {
                         rel.RelatedObjects.Add(loin);
+                        rel.RelatedObjectsType = IfcObjectTypeEnum.NOTDEFINED;
                         rel.RelatingProcess = i.New<IfcTask>(t =>
                         {
                             t.Name = "Stavební povolení";
@@ -103,6 +127,7 @@ namespace DDSS
                             ps.Name = "Základní informace o místnostech";
                             ps.ApplicableEntity = nameof(IfcSpace);
                             ps.HasPropertyTemplates.AddRange(new[] {
+                                // Simple property without unit
                                 i.New<IfcSimplePropertyTemplate>(p => {
                                     p.Name = "Název";
                                     p.Description = "Unikátní název místnosti. Pokud je budova rozdělena na bloky, je první písmeno označující blok budovy. Následující číslice obsahují podlaží a číslo místnosti.";
@@ -110,13 +135,11 @@ namespace DDSS
                                     p.TemplateType = IfcSimplePropertyTemplateTypeEnum.P_SINGLEVALUE;
                                     
                                 }),
+                                // simple property with unit m² and referenced document. There may be many documents as well
                                 i.New<IfcSimplePropertyTemplate>(p => {
                                     p.Name = "Plocha";
                                     p.PrimaryMeasureType = nameof(IfcAreaMeasure);
-                                    p.PrimaryUnit = i.New<IfcSIUnit>(t => {
-                                        t.Name = IfcSIUnitName.SQUARE_METRE;
-                                        t.UnitType = IfcUnitEnum.AREAUNIT;
-                                    });
+                                    p.PrimaryUnit = units["m2"];     //      <----------  All units have to be defined in compile time in the code
                                     p.TemplateType = IfcSimplePropertyTemplateTypeEnum.P_SINGLEVALUE;
                                     i.New<IfcRelAssociatesDocument>(rd => {
                                         rd.RelatedObjects.Add(p);
@@ -127,7 +150,26 @@ namespace DDSS
 
                                         });
                                     });
+                                }),
+
+                                // simple property with complex unit W / m² · K
+                                i.New<IfcSimplePropertyTemplate>(p => {
+                                    p.Name = "Thermal admittance";
+                                    p.PrimaryMeasureType = nameof(IfcThermalAdmittanceMeasure);
+                                    p.PrimaryUnit = units["W/m2.K"];
+                                    p.TemplateType = IfcSimplePropertyTemplateTypeEnum.P_SINGLEVALUE;
+                                }),
+
+                                // property enumeration
+                                i.New<IfcSimplePropertyTemplate>(p => {
+                                    p.Name = "Den konání";
+                                    p.PrimaryMeasureType = nameof(IfcLabel);  //    <------------- just use this for enumerations
+                                    p.TemplateType = IfcSimplePropertyTemplateTypeEnum.P_ENUMERATEDVALUE;  //   <----- this indicates it is an enumeration
+                                    p.Enumerators = enums.GetOrAdd("Dny v týdnu", new [] { "Pondělí", "Úterý", "Středa", "Čtvrtek", "Pátek", "Sobota", "Neděle"});  //      <--------values from DB
+                                    // or use p.Enumerators = enums.GetOrAdd("987jfgh-8978jfgh-jghjf45", "Dny v týdnu", new [] { "Pondělí", "Úterý", "Středa", "Čtvrtek", "Pátek", "Sobota", "Neděle"});  //      <--------values from DB
+                                    // if the NAME IS NOT THE KEY. You can also add all enumerations somewhere else and only get them by key here: enums["987jfgh-8978jfgh-jghjf45"]
                                 })
+
                                 // .... add all properties here
                             });
                         }));
@@ -137,8 +179,16 @@ namespace DDSS
                     txn.Commit();
                 }
 
-                model.SaveAs("DDSS.ifc");
-                //model.SaveAs("DDSS.ifcxml");
+                // validate schema and proper units
+                var validator = new Validator();
+                var valid = validator.Check(model);
+                if (!valid)
+                    throw new System.Exception("Invalid model shouldn't be stored and used for any purposes.");
+
+                model.SaveAs(ifcFile);
+
+                //var xmlFile = Path.ChangeExtension(ifcFile, ".ifcxml");
+                //model.SaveAs(xmlFile);
             }
         }
     }
---
+++ DDSS/Utils/Validator.cs
@@ -0,0 +1,328 @@
+﻿using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xbim.Common;
+using Xbim.Common.Enumerations;
+using Xbim.Common.ExpressValidation;
+using Xbim.Common.Metadata;
+using Xbim.Ifc4.Interfaces;
+
+namespace DDSS.Utils
+{
+    public class Validator
+    {
+        public bool Check(IModel model)
+        {
+            // check for parser exceptions
+            var v = new Xbim.Common.ExpressValidation.Validator
+            {
+                ValidateLevel = ValidationFlags.All,
+                CreateEntityHierarchy = true
+            };
+            var schemaErrors = v.Validate(model.Instances);
+            var templateErrors = CheckPropertyTemplateTypesAndUnits(model);
+            var propErrors = CheckPropertyUnits(model);
+
+            foreach (var err in
+                schemaErrors
+                .Concat(templateErrors)
+                .Concat(propErrors))
+            {
+                var identity = err.Item.GetType().Name;
+                if (err.Item is IPersistEntity entity)
+                {
+                    identity = $"#{entity.EntityLabel}={entity.ExpressType.ExpressName}";
+                }
+                var msg = new StringBuilder();
+                msg.AppendLine($"{identity} is invalid.");
+                var details = new Stack<ValidationResult>(err.Details);
+                while (details.Any())
+                {
+                    var detail = details.Pop();
+                    foreach (var d in detail.Details)
+                        details.Push(d);
+
+                    var report = detail.Message;
+                    if (string.IsNullOrWhiteSpace(report))
+                        report = detail.Report();
+                    msg.AppendLine("    " + report);
+
+                    if (detail.IssueType == ValidationFlags.EntityWhereClauses || detail.IssueType == ValidationFlags.TypeWhereClauses)
+                    {
+                        var source = detail.IssueSource.Split('.')[0].ToLower();
+                        msg.AppendLine($"http://www.buildingsmart-tech.org/ifc/IFC4/Add2/html/link/{source}.htm");
+                    }
+                }
+                Log.Error(msg.ToString());
+            }
+
+            return !schemaErrors.Any() && !templateErrors.Any() && !propErrors.Any();
+        }
+
+        private IEnumerable<ValidationResult> CheckPropertyUnits(IModel model)
+        {
+            var properties = model.Instances.OfType<IIfcSimpleProperty>();
+            ValidationResult detail = null;
+            foreach (var property in properties)
+            {
+                var err = new ValidationResult
+                {
+                    IssueSource = "Template Types and Units validation",
+                    Item = property,
+                    IssueType = ValidationFlags.Properties
+                };
+
+                if (property is IIfcPropertySingleValue single)
+                {
+                    if (single.NominalValue == null)
+                        continue;
+                    if ((detail = CheckUnit(single.NominalValue.GetType().Name, single.Unit, property)) != null)
+                        err.AddDetail(detail);
+                    continue;
+                }
+
+                if (property is IIfcPropertyListValue list)
+                {
+                    if (list.ListValues.FirstOrDefault() == null)
+                        continue;
+                    if ((detail = CheckUnit(list.ListValues.FirstOrDefault().GetType().Name, list.Unit, property)) != null)
+                        err.AddDetail(detail);
+                    continue;
+                }
+
+                if (property is IIfcPropertyBoundedValue bounded)
+                {
+                    if (bounded.UpperBoundValue != null)
+                        if ((detail = CheckUnit(bounded.UpperBoundValue.GetType().Name, bounded.Unit, property)) != null)
+                            err.AddDetail(detail);
+                    if (bounded.SetPointValue != null)
+                        if ((detail = CheckUnit(bounded.SetPointValue.GetType().Name, bounded.Unit, property)) != null)
+                            err.AddDetail(detail);
+                    if (bounded.LowerBoundValue != null)
+                        if ((detail = CheckUnit(bounded.LowerBoundValue.GetType().Name, bounded.Unit, property)) != null)
+                            err.AddDetail(detail);
+                    continue;
+                }
+
+                if (property is IIfcPropertyTableValue table)
+                {
+                    if (table.DefinedValues.FirstOrDefault() != null)
+                        if ((detail = CheckUnit(table.DefinedValues.FirstOrDefault().GetType().Name, table.DefinedUnit, property)) != null)
+                            err.AddDetail(detail);
+                    if (table.DefiningValues.FirstOrDefault() != null)
+                        if ((detail = CheckUnit(table.DefiningValues.FirstOrDefault().GetType().Name, table.DefiningUnit, property)) != null)
+                            err.AddDetail(detail);
+                    continue;
+                }
+
+                if (err.Details != null && err.Details.Any())
+                    yield return err;
+
+            }
+        }
+
+        private IEnumerable<ValidationResult> CheckPropertyTemplateTypesAndUnits(IModel model)
+        {
+            var templates = model.Instances.OfType<IIfcSimplePropertyTemplate>();
+            ValidationResult detail = null;
+            foreach (var property in templates)
+            {
+                if (!property.TemplateType.HasValue)
+                {
+                    // if template type is not defined there is no point in validation
+                    continue;
+                }
+
+                var err = new ValidationResult
+                {
+                    IssueSource = "Template Types and Units validation",
+                    Item = property,
+                    IssueType = ValidationFlags.Properties
+                };
+
+                switch (property.TemplateType.Value)
+                {
+                    case IfcSimplePropertyTemplateTypeEnum.P_SINGLEVALUE:
+                    case IfcSimplePropertyTemplateTypeEnum.P_LISTVALUE:
+                        if ((detail = CheckMeasureType(property.PrimaryMeasureType, model.Metadata, false)) != null)
+                            err.AddDetail(detail);
+                        if (property.PrimaryUnit != null && (detail = CheckUnit(property.PrimaryMeasureType, property.PrimaryUnit, property)) != null)
+                            err.AddDetail(detail);
+                        break;
+
+                    case IfcSimplePropertyTemplateTypeEnum.P_ENUMERATEDVALUE:
+                        if (property.Enumerators == null)
+                        {
+                            detail = new ValidationResult
+                            {
+                                Message = "Enumerators must be defined",
+                                IssueType = ValidationFlags.Properties
+                            };
+                            err.AddDetail(detail);
+                        }
+                        break;
+
+                    case IfcSimplePropertyTemplateTypeEnum.P_TABLEVALUE:
+                    case IfcSimplePropertyTemplateTypeEnum.P_BOUNDEDVALUE:
+                        if ((detail = CheckMeasureType(property.PrimaryMeasureType, model.Metadata, false)) != null)
+                            err.AddDetail(detail);
+                        if ((detail = CheckMeasureType(property.SecondaryMeasureType, model.Metadata, false)) != null)
+                            err.AddDetail(detail);
+
+                        if (property.PrimaryUnit != null && (detail = CheckUnit(property.PrimaryMeasureType, property.PrimaryUnit, property)) != null)
+                            err.AddDetail(detail);
+                        if (property.SecondaryUnit != null && (detail = CheckUnit(property.SecondaryMeasureType, property.SecondaryUnit, property)) != null)
+                            err.AddDetail(detail);
+                        break;
+
+                    case IfcSimplePropertyTemplateTypeEnum.P_REFERENCEVALUE:
+                        if ((detail = CheckMeasureType(property.PrimaryMeasureType, model.Metadata, true)) != null)
+                            err.AddDetail(detail);
+                        break;
+
+                    case IfcSimplePropertyTemplateTypeEnum.Q_LENGTH:
+                    case IfcSimplePropertyTemplateTypeEnum.Q_AREA:
+                    case IfcSimplePropertyTemplateTypeEnum.Q_VOLUME:
+                    case IfcSimplePropertyTemplateTypeEnum.Q_COUNT:
+                    case IfcSimplePropertyTemplateTypeEnum.Q_WEIGHT:
+                    case IfcSimplePropertyTemplateTypeEnum.Q_TIME:
+                    default:
+                        break;
+                }
+
+                if (err.Details != null && err.Details.Any())
+                    yield return err;
+            }
+        }
+
+        private ValidationResult CheckUnit(string measureType, IIfcUnit unit, IPersistEntity entity)
+        {
+            if (string.IsNullOrWhiteSpace(measureType))
+                return null;
+
+            measureType = measureType.ToUpperInvariant();
+
+            // no kind is defined for some measures (boolean, text, monetary, date etc.)
+            if (!MeasureUnitMaps.UnitKinds.TryGetValue(measureType, out UnitKind kind))
+                return null;
+
+            switch (kind)
+            {
+                case UnitKind.IfcUnitEnum:
+                    {
+                        if (!MeasureUnitMaps.Units.TryGetValue(measureType, out IfcUnitEnum unitType))
+                            throw new ArgumentOutOfRangeException();
+                        if (unit != null)
+                        {
+                            if (!(unit is IIfcNamedUnit namedUnit) || namedUnit.UnitType != unitType)
+                            {
+                                return new ValidationResult
+                                {
+                                    IssueType = ValidationFlags.Properties,
+                                    Item = entity,
+                                    Message = $"Named unit of type {unitType.ToString()} must be defined for {measureType}",
+                                    IssueSource = "Units to Measure Types validation"
+                                };
+                            }
+                        }
+                        else
+                        {
+                            // try to find unit assignment
+                            unit = entity.Model.Instances
+                                .OfType<IIfcUnitAssignment>()
+                                .Select(a => a.Units.FirstOrDefault<IIfcNamedUnit>(u => u.UnitType == unitType))
+                                .FirstOrDefault();
+                            if (unit == null)
+                            {
+                                return new ValidationResult
+                                {
+                                    IssueType = ValidationFlags.Properties,
+                                    Item = entity,
+                                    Message = $"Named unit of type {unitType.ToString()} must be defined for {measureType} but it was not found directly or in global units assignment.",
+                                    IssueSource = "Units to Measure Types validation"
+                                };
+                            }
+                        }
+                    }
+                    break;
+                case UnitKind.IfcDerivedUnitEnum:
+                    {
+                        if (!MeasureUnitMaps.DerivedUnits.TryGetValue(measureType, out IfcDerivedUnitEnum unitType))
+                            throw new ArgumentOutOfRangeException();
+                        if (unit != null)
+                        {
+                            if (!(unit is IIfcDerivedUnit derivedUnit) || derivedUnit.UnitType != unitType)
+                            {
+                                return new ValidationResult
+                                {
+                                    IssueType = ValidationFlags.Properties,
+                                    Item = entity,
+                                    Message = $"Derived unit of type {unitType.ToString()} must be defined for {measureType}",
+                                    IssueSource = "Units to Measure Types validation"
+                                };
+                            }
+                        }
+                        else
+                        {
+                            // try to find unit assignment
+                            unit = entity.Model.Instances
+                                .OfType<IIfcUnitAssignment>()
+                                .Select(a => a.Units.FirstOrDefault<IIfcDerivedUnit>(u => u.UnitType == unitType))
+                                .FirstOrDefault();
+                            if (unit == null)
+                            {
+                                return new ValidationResult
+                                {
+                                    IssueType = ValidationFlags.Properties,
+                                    Item = entity,
+                                    Message = $"Derived unit of type {unitType.ToString()} must be defined for {measureType} but it was not found directly or in global units assignment.",
+                                    IssueSource = "Units to Measure Types validation"
+                                };
+                            }
+                        }
+                    }
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+            return null;
+        }
+
+        private ValidationResult CheckMeasureType(string typeName, ExpressMetaData metadata, bool reference)
+        {
+            // null or empty is a valid value
+            if (string.IsNullOrWhiteSpace(typeName))
+                return null;
+
+            typeName = typeName.ToUpperInvariant();
+            var err = new ValidationResult
+            {
+                IssueSource = "Measure type validation",
+                IssueType = ValidationFlags.Properties,
+
+            };
+            if (!metadata.TryGetExpressType(typeName, out ExpressType eType))
+            {
+                err.Message = $"Type {typeName} is not an IFC type.";
+                return err;
+            }
+
+            var type = eType.Type;
+            if (!reference && !typeof(IIfcValue).IsAssignableFrom(type))
+            {
+                err.Message = $"Type {typeName} is not applicable. This must be assignable to 'IfcValue'";
+                return err;
+            }
+
+            if (reference && !typeof(IIfcObjectReferenceSelect).IsAssignableFrom(type))
+            {
+                err.Message = $"Type {typeName} is not applicable. This must be assignable to 'IfcObjectReferenceSelect'";
+                return err;
+            }
+
+            return null;
+        }
+    }
+}
