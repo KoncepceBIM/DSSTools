de69c353f100b63862ed86e717d74cc3f9b4ce84
Martin Cerny <martin.cerny@xbim.cz>
2021-11-03 14:48:33 +0100

    feature: IFC Type and PredefinedType extension

--- LOIN.Server/Contracts/BreakdownItem.cs
+++ LOIN.Server/Contracts/BreakdownItem.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using LOIN;
+using Xbim.Ifc4.ExternalReferenceResource;
+using Xbim.Ifc4.Interfaces;
 
 namespace LOIN.Server.Contracts
 {
@@ -23,12 +25,20 @@ namespace LOIN.Server.Contracts
                 Children = query.Select(c => new BreakdownItem(c, onlyWithRequirements)).ToList();
             }
 
+            if (item.Entity is IfcClassificationReference cref)
+            {
+                IFCType = cref.GetIFCType();
+                IFCPredefinedType = cref.GetIFCPredefinedType();
+            }
 
         }
         public string Code { get; set; }
 
         public string NoteCS { get; set; }
         public string NoteEN { get; set; }
+
+        public string IFCType { get; set; }
+        public string IFCPredefinedType { get; set; }
 
         public List<BreakdownItem> Children { get; set; }
     }
---
+++ LOIN/Extensions/IfcMapLangExtension.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Xbim.Common;
+using Xbim.Ifc4.ExternalReferenceResource;
+using Xbim.Ifc4.Interfaces;
+using Xbim.Ifc4.Kernel;
+
+namespace LOIN
+{
+    public static class IfcTypeLangExtension
+    {
+        public const string identifier = "ifc_type_dictionary";
+        private const string ifc = "ifc";
+
+        private static LangCache GetCache(IModel model)
+        {
+            return model.GetCache(identifier, () => new LangCache(model, identifier));
+        }
+
+        public static string GetIFCType(this IfcExternalReference definition)
+        {
+            var c = GetCache(definition.Model);
+            return c.GetDescription(definition, ifc);
+        }
+
+        public static void SetIFCType(this IfcExternalReference definition, string type)
+        {
+            var c = GetCache(definition.Model);
+            c.SetDescription(definition, ifc, type);
+        }
+    }
+
+    public static class IfcPredefinedTypeLangExtension
+    {
+        public const string identifier = "ifc_predefined_type_dictionary";
+        private const string ifc = "ifc";
+
+        private static LangCache GetCache(IModel model)
+        {
+            return model.GetCache(identifier, () => new LangCache(model, identifier));
+        }
+
+        public static string GetIFCPredefinedType(this IfcExternalReference definition)
+        {
+            var c = GetCache(definition.Model);
+            return c.GetDescription(definition, ifc);
+        }
+
+        public static void SetIFCPredefinedType(this IfcExternalReference definition, string type)
+        {
+            var c = GetCache(definition.Model);
+            c.SetDescription(definition, ifc, type);
+        }
+    }
+}
